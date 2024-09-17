e9bf40094965cb78b6ce46506f6cf6a1edc7b07c
theoalll <theo.ananda@ui.ac.id>
2024-09-17 12:49:38 +0700

    try github action push to pws

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class ProductEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     product_name = models.CharField(max_length=100)
     price = models.IntegerField()
     product_description = models.TextField(max_length=255)
