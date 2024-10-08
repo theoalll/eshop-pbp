9249f126ea1386829c895f93e679cff882e64890
theoalll <theo.ananda@ui.ac.id>
2024-10-08 17:24:32 +0700

    add validation empty input before adding to database

--- main/forms.py
+++ main/forms.py
@@ -9,18 +9,18 @@ class ProductEntryForm(ModelForm):
         model = ProductEntry
         fields = ["product_name", "price", "product_description", "available_qty"]
 
-        def clean_product_name(self):
-            product_name = self.cleaned_data["product_name"]
-            return strip_tags(product_name)
+    def clean_product_name(self):
+        product_name = self.cleaned_data["product_name"]
+        return strip_tags(product_name)
 
-        def clean_price(self):
-            price = self.cleaned_data["price"]
-            return strip_tags(price)
+    def clean_price(self):
+        price = self.cleaned_data["price"]
+        return strip_tags(price)
 
-        def clean_product_description(self):
-            product_description = self.cleaned_data["product_description"]
-            return strip_tags(product_description)
+    def clean_product_description(self):
+        product_description = self.cleaned_data["product_description"]
+        return strip_tags(product_description)
 
-        def clean_available_qty(self):
-            available_qty = self.cleaned_data["available_qty"]
-            return strip_tags(available_qty)
+    def clean_available_qty(self):
+        available_qty = self.cleaned_data["available_qty"]
+        return strip_tags(available_qty)
